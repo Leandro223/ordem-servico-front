286cc4715daf8e5b9205f13950cbc88c608c1205
Baracho <baracholss@gmail.com>
2023-06-02 15:23:48 -0300

    autenticação de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +20,9 @@ export class LoginComponent implements OnInit{
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
     
-  constructor(private toast: ToastrService){}
+  constructor(private toast: ToastrService,
+              private service: AuthService,
+              private router: Router){}
 
 
   ngOnInit(): void {
@@ -26,8 +30,13 @@ export class LoginComponent implements OnInit{
   }
 
   logar(){
-    this.toast.error('Usuario e/ou senha inválido!', 'Login');
-    this.creds.senha = '';
+    this.service.authenticated(this.creds).subscribe(resposta => {
+      this.service.sucessfullLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+
+    }, () => {
+      this.toast.error('Usuario e/ou senha invalidos!');
+    })
   }
 
   validarCampos(): boolean {
